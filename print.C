1089f1afe7d3d65923149dcce390ad8e9d926c52
indhuvsb <36696610+indhuvsb@users.noreply.github.com>
2018-03-16 16:43:08 +0530

    hghtg.C
    good

---
+++ print.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    long num, temp, digit, sum = 0;
+ 
+    printf("Enter the number \n");
+    scanf("%ld", &num);
+    temp = num;
+    }
+    printf("Given number = %ld\n", temp);
+    printf("Sum of the digits %ld = %ld\n", temp, sum);
+    return 0;
+}
